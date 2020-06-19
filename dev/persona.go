7b43497f56d9a9467db80da91d6f7b28abac00ff
kaungmyatchanthar <kaungmyatchanthar.1@gmail.com>
2020-06-20 00:09:25 +0630

    add some fun text

--- dev/persona.go
+++ dev/persona.go
@@ -5,7 +5,7 @@ import "fmt"
 type person struct{}
 
 func (p person) Talk() {
-	fmt.Println("hello world")
+	fmt.Println("မင်္ဂလာပါ လောကကြီးရေ")
 }
 
 // Persona လို့ခေါ်တဲ့ symbol ကို export လုပ်လိုက်
--- monk/persona.go
+++ monk/persona.go
@@ -5,7 +5,7 @@ import "fmt"
 type person struct{}
 
 func (p person) Talk() {
-	fmt.Println("life is suffering")
+	fmt.Println("ငါသည် မုချသေရမည် အချိန်ပိုင်းသာလိုတော့သည်")
 }
 
 // Persona လို့ခေါ်တဲ့ symbol ကို export လုပ်လိုက်
